d9cb5e42b7588f89dfb30fd4ba2ddc280925a3ec
Gopal Meena <meenagopal24@gmail.com>
2025-05-01 23:43:07 +0530

    core setup

---
+++ core/models/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+}
+
+group = "com.devom.core.models"
+
+kotlin {
+    jvm()
+    jvmToolchain(17)
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+
+    }
+}
+
+android {
+    namespace = "com.devom.core"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
