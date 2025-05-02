e638a3d1173c387fa8222467193dea0969c1a8f9
Gopal Meena <meenagopal24@gmail.com>
2025-05-03 02:08:44 +0530

    feat: Implement Generate OTP functionality
    - Created `GenerateOtpUseCase` to handle OTP generation logic.
    - Added `generateOtp` function to `UserRepository` and its implementation.
    - Created `AuthEndpoints` for API endpoint definition and moved it into the server package.
    - Implemented `generateOtp` in `UserRemoteDataSource` for remote data fetching.
    - Renamed `UserRemoteDataSource` to a proper package name.
    - Deleted redundant `AuthEndpoints` file.

--- data/repository/src/commonMain/kotlin/com/devom/data/repository/user/UserRepository.kt
+++ data/repository/src/commonMain/kotlin/com/devom/data/repository/user/UserRepository.kt
@@ -1,6 +1,6 @@
 package com.devom.data.repository.user
 
-import auth.UserRemoteDataSourceImpl
+import com.devom.data.server.auth.UserRemoteDataSourceImpl
 import com.devom.models.auth.CreateUserRequest
 import com.devom.models.auth.CreateUserResponse
 import com.devom.models.auth.LoginWithOtpRequest
@@ -8,11 +8,11 @@ import com.devom.models.auth.UserResponse
 import com.devom.utils.flow.apiFlow
 import com.devom.utils.network.ResponseResult
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
 
 interface UserRepository {
     suspend fun addUser(user : CreateUserRequest) : Flow<ResponseResult<CreateUserResponse>>
     suspend fun loginWithOtp(credentials : LoginWithOtpRequest) : Flow<ResponseResult<UserResponse>>
+    suspend fun generateOtp(mobileNo : String) : Flow<ResponseResult<String>>
 }
 
 class UserRepositoryImpl : UserRepository {
@@ -28,6 +28,10 @@ class UserRepositoryImpl : UserRepository {
 
     override suspend fun loginWithOtp(credentials: LoginWithOtpRequest): Flow<ResponseResult<UserResponse>> = apiFlow {
         remoteDataSource.loginWithOtp(credentials)
+    }
+
+    override suspend fun generateOtp(mobileNo: String): Flow<ResponseResult<String>> = apiFlow {
+        remoteDataSource.generateOtp(mobileNo)
     }
 
 }
--- data/server/src/commonMain/kotlin/com/devom/data/server/auth/UserRemoteDataSource.kt
+++ data/server/src/commonMain/kotlin/com/devom/data/server/auth/UserRemoteDataSource.kt
@@ -1,4 +1,4 @@
-package auth
+package com.devom.data.server.auth
 
 import com.devom.models.auth.CreateUserRequest
 import com.devom.models.auth.CreateUserResponse
@@ -7,7 +7,8 @@ import com.devom.models.auth.UserResponse
 import com.devom.network.NetworkClient
 import com.devom.network.utils.toResponseResult
 import com.devom.utils.network.ResponseResult
-import endpoints.AuthEndpoints
+import com.devom.data.server.endpoints.AuthEndpoints
+import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import kotlinx.coroutines.flow.Flow
@@ -15,6 +16,7 @@ import kotlinx.coroutines.flow.Flow
 interface UserRemoteDataSource {
    suspend fun signUp(user: CreateUserRequest): Flow<ResponseResult<CreateUserResponse>>
    suspend fun loginWithOtp(credentials : LoginWithOtpRequest) : Flow<ResponseResult<UserResponse>>
+   suspend fun generateOtp(mobileNo : String) : Flow<ResponseResult<String>>
 }
 
 class UserRemoteDataSourceImpl : UserRemoteDataSource {
@@ -32,7 +34,14 @@ class UserRemoteDataSourceImpl : UserRemoteDataSource {
      * @return Flow<ResponseResult<UserResponse>>
      */
     override suspend fun loginWithOtp(credentials: LoginWithOtpRequest): Flow<ResponseResult<UserResponse>> =
-        ktorClient.post { setBody(credentials) }.toResponseResult()
+        ktorClient.post(AuthEndpoints.LoginWithOtp.path) { setBody(credentials) }.toResponseResult()
+
+    /**
+     * @param mobileNo
+     * @return Flow<ResponseResult<String>>
+     */
+    override suspend fun generateOtp(mobileNo: String): Flow<ResponseResult<String>> =
+        ktorClient.get(AuthEndpoints.GenerateOtp.path) { setBody(mobileNo) }.toResponseResult()
 
 
 }
