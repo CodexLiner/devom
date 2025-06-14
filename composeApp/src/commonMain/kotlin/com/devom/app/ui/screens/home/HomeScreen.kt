bc8fb850189b0d6835aa7b9c09ffa5e2ec954d90
Gopal Meena <meenagopal24@gmail.com>
2025-06-15 01:14:06 +0530

    feat: Display transaction chart in HomeScreen
    - Added `BarChartExample` to `HomeScreenContent` to display a bar chart of transactions.
    - Updated `HomeScreenViewModel` to fetch user profile and wallet transactions.
    - The `BarChartExample` is now populated with data from the `transactions` state flow.

--- composeApp/src/commonMain/kotlin/com/devom/app/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/devom/app/ui/screens/home/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.devom.app.BarChartExample
 import com.devom.app.theme.backgroundColor
 import com.devom.app.theme.blackColor
 import com.devom.app.theme.text_style_h5
@@ -25,11 +26,8 @@ import com.devom.app.ui.components.AppBar
 import com.devom.app.ui.components.NoContentView
 import com.devom.app.ui.navigation.Screens
 import com.devom.app.ui.screens.booking.components.BookingCard
-import com.devom.network.NetworkClient
-import com.devom.utils.Application
 import org.jetbrains.compose.resources.painterResource
 import pandijtapp.composeapp.generated.resources.Res
-import pandijtapp.composeapp.generated.resources.ic_logout
 import pandijtapp.composeapp.generated.resources.ic_notification
 
 @Composable
@@ -59,6 +57,7 @@ fun HomeScreen(navHostController: NavHostController , onNavigationIconClick: () 
 @Composable
 fun HomeScreenContent(viewModel: HomeScreenViewModel, navHostController: NavHostController) {
     val bookings = viewModel.bookings.collectAsState()
+    val transactions = viewModel.transactions.collectAsState()
     if (bookings.value.isEmpty()) NoContentView(
         message = "No Bookings Found",
         title = null,
@@ -74,6 +73,11 @@ fun HomeScreenContent(viewModel: HomeScreenViewModel, navHostController: NavHost
             ),
             modifier = Modifier.fillMaxSize()
         ) {
+
+            item {
+                BarChartExample(transactions.value.transactions)
+            }
+
             item {
                 Text(
                     text = "Recent Bookings",
--- composeApp/src/commonMain/kotlin/com/devom/app/ui/screens/home/HomeScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/devom/app/ui/screens/home/HomeScreenViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.devom.Project
 import com.devom.app.models.ApplicationStatus
+import com.devom.models.auth.UserRequestResponse
+import com.devom.models.payment.GetWalletTransactionsResponse
 import com.devom.models.slots.GetBookingsResponse
 import com.devom.models.slots.UpdateBookingStatusInput
 import com.devom.utils.cachepolicy.CachePolicy
 import com.devom.utils.network.onResult
+import com.devom.utils.network.withSuccess
 import com.devom.utils.network.withSuccessWithoutData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,12 +21,43 @@ class HomeScreenViewModel : ViewModel(){
     private val _bookings = MutableStateFlow<List<GetBookingsResponse>>(listOf())
     val bookings: StateFlow<List<GetBookingsResponse>> = _bookings
 
+    private val _user = MutableStateFlow<UserRequestResponse?>(null)
+    val user = _user
+
+
+    private val _transactions = MutableStateFlow(GetWalletTransactionsResponse())
+    val transactions = _transactions
+    init {
+        getUserProfile()
+    }
 
     fun getBookings() {
         viewModelScope.launch {
             Project.pandit.getPanditBookingsUseCase.invoke(cachePolicy = CachePolicy.CacheAndNetwork).collect {
                 it.onResult {
                     _bookings.value = it.data
+                }
+            }
+        }
+    }
+
+    fun getUserProfile() {
+        viewModelScope.launch {
+            Project.user.getUserProfileUseCase.invoke().collect {
+                it.withSuccess {
+                    _user.value = it.data
+                    getTransactions(_user.value?.userId.toString())
+                }
+            }
+        }
+    }
+
+
+    fun getTransactions(userId : String) {
+        viewModelScope.launch {
+            Project.payment.getWalletTransactionsUseCase.invoke(userId).collect {
+                it.onResult {
+                    _transactions.value = it.data
                 }
             }
         }
